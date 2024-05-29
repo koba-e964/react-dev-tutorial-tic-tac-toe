0038cf71dfaffb35856ae711357b1ba0c97ec07a
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2024-05-29 23:45:41 +0900

    Finish Adding time travel

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,66 @@
 import React, { useState } from "react";
 import { calculateWinner } from "./game.ts";
+import { on } from "events";
 
-export default function Board(): JSX.Element {
-  const [isXNext, setIsXNext] = useState<boolean>(true);
-  const [squares, setSquares] = useState<Array<string | null>>(
-    Array(9).fill(null)
+export default function Game(): JSX.Element {
+  const [history, setHistory] = useState<Array<Array<string | null>>>([
+    Array(9).fill(null),
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: Array<string | null>): void {
+    setHistory([...history.slice(0, currentMove + 1), nextSquares]);
+    setCurrentMove(currentMove + 1);
+  }
+
+  function jumpTo(moveIndex: number): void {
+    setCurrentMove(moveIndex);
+  }
+
+  const moves = history.map((squares: (string | null)[], moveIndex: number) => {
+    let description: string;
+    if (moveIndex > 0) {
+      description = "Go to move #" + moveIndex;
+    } else {
+      description = "Go to game start";
+    }
+    if (moveIndex === currentMove) {
+      return <li key={moveIndex}>{"You are on move #" + moveIndex}</li>;
+    }
+    return (
+      <li key={moveIndex}>
+        <button onClick={() => jumpTo(moveIndex)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
+}
 
+function Board({
+  xIsNext,
+  squares,
+  onPlay,
+}: {
+  xIsNext: boolean;
+  squares: Array<string | null>;
+  onPlay: (_: Array<string | null>) => void;
+}): JSX.Element {
   const winner = calculateWinner(squares);
   const status = winner
     ? `Winner: ${winner}`
-    : `Next player: ${isXNext ? "X" : "O"}`;
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   function handleSquareClick(i: number): void {
     if (squares[i] !== null || calculateWinner(squares) !== null) {
@@ -18,9 +68,8 @@ export default function Board(): JSX.Element {
     }
 
     const newSquares = squares.slice();
-    newSquares[i] = isXNext ? "X" : "O";
-    setSquares(newSquares);
-    setIsXNext(!isXNext);
+    newSquares[i] = xIsNext ? "X" : "O";
+    onPlay(newSquares);
   }
 
   return (
