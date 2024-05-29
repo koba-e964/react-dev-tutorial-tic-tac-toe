3b545ab3b647885ede183ea4177aa8ec10d2aad9
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2024-05-29 23:04:28 +0900

    Finish Completing the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,59 @@
 import React, { useState } from "react";
+import { calculateWinner } from "./game.ts";
 
 export default function Board(): JSX.Element {
+  const [isXNext, setIsXNext] = useState<boolean>(true);
+  const [squares, setSquares] = useState<Array<string | null>>(
+    Array(9).fill(null)
+  );
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? "X" : "O"}`;
+
+  function handleSquareClick(i: number): void {
+    if (squares[i] !== null || calculateWinner(squares) !== null) {
+      return;
+    }
+
+    const newSquares = squares.slice();
+    newSquares[i] = isXNext ? "X" : "O";
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
       </div>
     </>
   );
 }
 
-function Square(): JSX.Element {
-  const [value, setValue] = useState<string | null>(null);
-
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}): JSX.Element {
   function handleClick(): void {
-    setValue("X");
+    onSquareClick();
   }
   return (
     <button className="square" onClick={handleClick}>
---
+++ src/game.ts
@@ -0,0 +1,21 @@
+export function calculateWinner(board: Array<string | null>): string | null {
+  const lines = [
+    [0, 1, 2], // horizontal
+    [3, 4, 5], // horizontal
+    [6, 7, 8], // horizontal
+    [0, 3, 6], // vertical
+    [1, 4, 7], // vertical
+    [2, 5, 8], // vertical
+    [0, 4, 8], // diagonal
+    [2, 4, 6], // diagonal
+  ];
+
+  for (const line of lines) {
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+}
